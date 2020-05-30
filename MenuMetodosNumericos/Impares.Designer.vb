bd6d9472374d9817279c3bdeb93ac13ddfccba75
Jordana <jordanazg@outlook.com>
2020-05-30 16:41:13 -0500

    Hola cara de bola

--- MenuMetodosNumericos/Impares.Designer.vb
+++ MenuMetodosNumericos/Impares.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Impares
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,11 +35,21 @@ Partial Class Impares
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Impares"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(332, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Impares
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Impares"
         Me.Text = "Impares"
@@ -48,4 +59,5 @@ Partial Class Impares
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
